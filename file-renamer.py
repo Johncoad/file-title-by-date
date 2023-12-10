56ae695640cd5589f91801b734056c6ad53660c9
Corey <134085795+Johncoad@users.noreply.github.com>
2023-12-10 15:40:53 -0500

    Update file-renamer.py
    rewrote script to add mp4, logging, and user prompting

--- file-renamer.py
+++ file-renamer.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import exifread
 import subprocess
 
+# Function to extract date and time from image EXIF data
 def get_image_date_taken(file_path):
     """Extract date and time from image EXIF data."""
     with open(file_path, 'rb') as f:
@@ -12,22 +13,77 @@ def get_image_date_taken(file_path):
             return str(date_taken_tag)
     return None
 
+# Function to extract date and time from video metadata (MP4 format)
 def get_video_date_taken(file_path):
-    """Extract date and time from video metadata."""
-    # This is a placeholder function. Actual implementation depends on the video format and metadata.
+    """Extract date and time from video metadata (MP4 format)."""
+    try:
+        # Use the 'ffprobe' tool from FFmpeg to extract creation time from MP4 video metadata
+        cmd = ['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-show_entries', 'stream_tags=creation_time', '-of', 'default=noprint_wrappers=1:nokey=1', file_path]
+        creation_time = subprocess.check_output(cmd, stderr=subprocess.STDOUT, universal_newlines=True).strip()
+        if creation_time:
+            return creation_time
+    except Exception as e:
+        print(f"Error extracting video creation time from {file_path}: {str(e)}")
     return None
 
-def rename_files_in_directory(directory):
-    for filename in os.listdir(directory):
+# Function to rename files in a directory based on date and time
+def rename_files_in_directory(source_directory, destination_directory):
+    renamed_files = []  # To store the successfully renamed files
+    not_renamed_files = []  # To store files that were not renamed
+    
+    for filename in os.listdir(source_directory):
+        source_path = os.path.join(source_directory, filename)
+        
         if filename.lower().endswith('.jpg'):
-            file_path = os.path.join(directory, filename)
-            date_taken = get_image_date_taken(file_path)
+            date_taken = get_image_date_taken(source_path)
             if date_taken:
-                new_name = datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S').strftime('%Y-%m-%d_%H-%M-%S') + '.jpg'
-                os.rename(file_path, os.path.join(directory, new_name))
+                try:
+                    # Convert the extracted date and time to the desired format and add it to the new filename
+                    new_name = datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S').strftime('%Y%m%d_%H%M%S') + '.jpg'
+                    destination_path = os.path.join(destination_directory, new_name)
+                    
+                    # Rename the file
+                    os.rename(source_path, destination_path)
+                    renamed_files.append(filename)
+                except Exception as e:
+                    not_renamed_files.append(filename)
+                    print(f"Error renaming {filename}: {str(e)}")
         elif filename.lower().endswith('.mp4'):
-            # Similar handling for MP4 files using get_video_date_taken
+            date_taken = get_video_date_taken(source_path)
+            if date_taken:
+                try:
+                    # Convert the extracted date and time to the desired format and add it to the new filename
+                    new_name = datetime.strptime(date_taken, '%Y-%m-%dT%H:%M:%S').strftime('%Y%m%d_%H%M%S') + '.mp4'
+                    destination_path = os.path.join(destination_directory, new_name)
+                    
+                    # Rename the file
+                    os.rename(source_path, destination_path)
+                    renamed_files.append(filename)
+                except Exception as e:
+                    not_renamed_files.append(filename)
+                    print(f"Error renaming {filename}: {str(e)}")
+        else:
+            # Handle other file types or extensions if needed
             pass  # Placeholder
+    
+    return renamed_files, not_renamed_files
 
-# Replace '/path/to/directory' with the path of your target directory
-rename_files_in_directory('/path/to/directory')
+if __name__ == "__main__":
+    # Get user input for source and destination directories
+    source_directory = input("Enter the source directory path: ")
+    destination_directory = input("Enter the destination directory path: ")
+
+    # Call the rename_files_in_directory function with user-provided directories
+    renamed_files, not_renamed_files = rename_files_in_directory(source_directory, destination_directory)
+
+    # Display status messages and lists of renamed and not renamed files
+    print("Status:")
+    if renamed_files:
+        print("Renamed files:")
+        for filename in renamed_files:
+            print(f"- {filename}")
+    if not_renamed_files:
+        print("Files not renamed:")
+        for filename in not_renamed_files:
+            print(f"- {filename}")
+    print("Script completed successfully.")
